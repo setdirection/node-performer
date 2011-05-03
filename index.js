0636ea3ec57ef6f2df8a76462d99db745a0dd0fb
kpdecker <kpdecker@gmail.com>
2011-05-03 02:14:55 -0500

    Export bundled plugins on the root module rather than a plugin object.

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ fs = require("fs");
 
 module.exports = exports = require("./lib/performer.coffee");
 
-exports.plugins = {};
-
 /*
  * Load all bundled plugins on demand.
  */
 fs.readdirSync(__dirname + '/lib/plugins').forEach(function(filename){
   if (/\.(coffee|js)$/.test(filename)) {
     var name = filename.substr(0, filename.lastIndexOf('.'));
-    exports.plugins.__defineGetter__(name, function(){
+    exports.__defineGetter__(name, function(){
       return require('./lib/plugins/' + filename);
     });
   }
